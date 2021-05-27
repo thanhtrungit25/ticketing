774e87d57775875706f16124f4cc06970bfc5040
Trung DG <thanhtrung.it25@gmail.com>
2021-05-27 20:17:15 +0700

    Common request validation middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from 'express'
 import jwt from 'jsonwebtoken'
-import { body, validationResult } from 'express-validator'
+import { body } from 'express-validator'
 import { BadRequestError } from '../errors/bad-request-error'
-import { RequestValidationError } from '../errors/request-validation-error'
 import { User } from '../models/user'
+import { validateRequest } from '../middlewares/validate-request'
 
 const router = express.Router()
 
@@ -17,14 +17,8 @@ router.post('/api/users/signup',
     .isLength({ min: 4, max: 20 })
     .withMessage('Password must be between 4 - 20 characters')
 ],
+validateRequest,
 async (req: Request, res: Response) => {
-  // Finds the validation errors in this request
-  const errors = validationResult(req)
-
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array())
-  }
-
   const { email, password } = req.body
 
   const existingUser = await User.findOne({ email })
