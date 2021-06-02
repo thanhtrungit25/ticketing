ff3246367bf602748a6bf3f6132d179b89ab22e1
Trung DG <thanhtrung.it25@gmail.com>
2021-06-02 21:39:18 +0700

    TDD create ticket with new ticket model

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import { app } from '../../app'
+import { Ticket } from '../../models/ticket'
 
 it('has a route handler listening to /api/tickets for post request', async () => {
   const response = await request(app)
@@ -65,5 +66,19 @@ it('return an error if an invalid price is provided', async () => {
 })
 
 it('create a ticket with valid inputs', async () => {
+  let tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(0)
 
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'sfsdfs',
+      price: 20
+    })
+    .expect(201)
+
+  tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(1)
+  expect(tickets[0].price).toEqual(20)
 })
