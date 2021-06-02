8e49c61ab60682b70442d917d789044cce999672
Trung DG <thanhtrung.it25@gmail.com>
2021-06-02 20:57:40 +0700

    Validate title and price with test TDD

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -22,16 +22,46 @@ it('return a status other than 404 if the user is signed in', async () => {
     .set('Cookie', global.signin())
     .send({})
 
-  console.log(response.status)
   expect(response.status).not.toEqual(401)
 })
 
 it('return an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10
+    })
+    .expect(400)
 
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10
+    })
+    .expect(400)
 })
 
 it('return an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'sfsdfs',
+      price: ''
+    })
+    .expect(400)
 
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'sfsdfs',
+      price: -10
+    })
+    .expect(400)
 })
 
 it('create a ticket with valid inputs', async () => {
--- tickets/src/routes/__test__/new.ts
+++ tickets/src/routes/__test__/new.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response } from 'express'
-import { requireAuth } from '@mictickets/common'
+import { body } from 'express-validator'
+import { requireAuth, validateRequest } from '@mictickets/common'
 
 const router = express.Router()
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
+router.post('/api/tickets', requireAuth, [
+  body('title')
+    .not()
+    .isEmpty()
+    .withMessage('Title must be valid'),
+  body('price')
+    .isFloat({gt: 0})
+    .withMessage('Price must be greater than 0')
+], validateRequest, (req: Request, res: Response) => {
   console.log('create tickets')
   res.sendStatus(200)
 })
